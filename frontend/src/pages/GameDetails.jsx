47c665b9ad3ad4d14d46cf7e3f7a223453602909
Gustavo França <gustavo.santos568.franca@gmail.com>
2025-10-09 06:01:11 -0300

    deu erro

--- frontend/src/pages/GameDetails.jsx
+++ frontend/src/pages/GameDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { api } from "../services/api";
+import api, { imgUrl } from "../services/api";
 
 export default function GameDetails() {
   const { id } = useParams();
