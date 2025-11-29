5d5ccda9bb343f01061d2b5028e9a260a6e7b2bd
Gustavo França <gustavo.santos568.franca@gmail.com>
2025-11-29 01:26:26 -0300

    att

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0712a37b81744d3d95908c209a879fdacc73b5fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0748a7124be29c18196657f9876853f0f28362")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
